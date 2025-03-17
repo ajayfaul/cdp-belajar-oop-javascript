77d8aec7d93021032242e27e8e6af60677fa79c1
ajayfaul <muhammadfajarm@gmail.com>
2025-03-17 09:39:17 +0700

    membuat object dengan new / prototype constructor function

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // Membuat object dengan factory function
 
-const hex = (r, g, b) => {
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+
 
 const rgb = (r,g,b) => {
     return `rgb(${r}, ${g}, ${b})`;
@@ -26,4 +24,26 @@ function convertColor(r, g, b) {
     }
 
     return color;
+}
+
+// membuat object dengan constructor function
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+
+Color.prototype.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+Color.prototype.hex = function () {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+Color.prototype.rgba = function (a = 1.0) {
+    const { r, g, b } = this;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
